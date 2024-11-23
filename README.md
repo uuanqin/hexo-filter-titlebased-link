82140145539f4178ec6da129b61c6cb984b492e4
wuanqin <wuanqin@mail.ustc.edu.cn>
2024-11-23 21:45:17 +0800

    0.2.0 add options

--- README.md
+++ README.md
@@ -8,9 +8,9 @@ This plugin makes you easy to use wiki links (its form may be `[[Title]]` or `[[
 It would be more useful when you use [Obsidian](https://obsidian.md/) to manage your blogs.
 
 > [!NOTE]
-> - Make sure that no more than one vertical bar (`|`) in a wiki link.
-> - If one vertical bar included, `Alias` should not be empty.
-> - In addition, the title of all your articles should be unique.
+> - Ensure that no more than one vertical bar (`|`) is included in a wiki link.
+> - If one vertical bar is included, `Alias` should not be empty.
+> - Additionally, ensure that the titles of all your articles are unique.
 
 ## Installation
 
@@ -19,6 +19,14 @@ npm install hexo-filter-titlebased-link --save
 ```
 
 ## Usage
+
+To configure this plugin, add these contents in the `_config.yml`:
+
+```yaml
+titlebased_link:
+  enable: true   # enable this plugin
+  class_name: "" # class name of wiki links
+```
 
 Hexo sets every post a [Permalinks](https://hexo.io/docs/permalinks.html), which can be configured in the `_config.yml`.
 
@@ -29,7 +37,7 @@ Here is an example:
 permalink: p/:year/:month/:day/:hour/:minute/:second/
 ```
 
-Assume that you have a post named `my_post_1` whose permalink is `p/2024/04/12/14/18/50/`. 
+Assume you have a post named `my_post_1` whose permalink is `p/2024/04/12/14/18/50/`. 
 In another post named `my_post_2`, you wrote a wiki link to `my_post_1`:
 
 ```markdown
@@ -40,12 +48,18 @@ you can see my last post by clicking this link: [[my_post_1]],
 or [[my_post_1|this link]].
 ```
 
-After rendering by Hexo, the result html file of `my_post_2.md` will be:
+After rendering by Hexo, the resultant HTML file of `my_post_2.md` will be:
 
 ```html
 <p>This is my_post_2, </p>
 <p>you can see my last post by clicking this link: <a href="/p/2024/04/12/14/18/50/">my_post_1</a>,</p>
 <p>or <a href="/p/2024/04/12/14/18/50/">this link</a>.</p>
+```
+
+If you specify a class name (e.g., `my-class`) in the options, the rendering result will be:
+
+```html
+<a class="my-class" href="/p/2024/04/12/14/18/50/">this link</a>
 ```
 
 ## Related Hexo Plugins
--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 'use strict';
 
+const config = hexo.config.titlebased_link = Object.assign({
+  enable: false,
+  class_name: ""
+}, hexo.config.titlebased_link);
+
 const log = require('hexo-log').default({
   debug: false,
   silent: false
@@ -7,33 +12,35 @@ const log = require('hexo-log').default({
 
 const cachedPost = {};
 let lastPost;
-hexo.extend.filter.register('post_permalink', function(data){
-  lastPost = data;
-  return data;
-}, 1);
 
-hexo.extend.filter.register('post_permalink', function(permalink) {
-  if (lastPost) {
-    const fileName = lastPost.source.match(/[^/]*$/)[0].replace(/\.md$/, '');
-    cachedPost[fileName] = permalink;
-  }
-  return permalink;
-}, 25);
 
-hexo.extend.filter.register("before_post_render", (post) => {
-  const re = /\[\[([^\*"\\/<>:?\[\]\|]+)\|?([^\*"\\/<>:|?\[\]]*)?\]\]/g;
-  post.content = post.content.replace(re, function(match, p1,p2) {
-    const fileName = decodeURI(p1);
-    if (cachedPost[fileName]) {
-      log.info("hexo-filter-titlebased-link: Replace -",fileName);
-      return p2?`<a href="/${cachedPost[fileName]}">${decodeURI(p2)}</a>`:`<a href="/${cachedPost[fileName]}">${fileName}</a>`;
+if (config.enable) {
+
+  hexo.extend.filter.register('post_permalink', function (data) {
+    lastPost = data;
+    return data;
+  }, 1);
+
+  hexo.extend.filter.register('post_permalink', function (permalink) {
+    if (lastPost) {
+      const fileName = lastPost.source.match(/[^/]*$/)[0].replace(/\.md$/, '');
+      cachedPost[fileName] = permalink;
     }
-    return match;
+    return permalink;
+  }, 25);
+
+  hexo.extend.filter.register("before_post_render", (post) => {
+    const re = /\[\[([^\*"\\/<>:?\[\]\|]+)\|?([^\*"\\/<>:|?\[\]]*)?\]\]/g;
+    post.content = post.content.replace(re, function (match, p1, p2) {
+      const fileName = decodeURI(p1);
+      const className = config.class_name==="" ? "" : `class="`+config.class_name+`" `;
+
+      if (cachedPost[fileName]) {
+        log.info("hexo-filter-titlebased-link: Replace -", fileName);
+        return p2 ? `<a ${className}href="/${cachedPost[fileName]}">${decodeURI(p2)}</a>` : `<a href="/${cachedPost[fileName]}">${fileName}</a>`;
+      }
+      return match;
+    });
+    return post;
   });
-  return post;
-});
-
-
-
-
-
+}
